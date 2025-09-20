e3878f6111c82309acb8914c5cb89f34bef5191b
Emil Popa <emilpopa24@yahoo.com>
2025-09-20 18:45:05 +0300

    some adjustments regarding IT

--- src/test/java/com/emil/cicd/repository/UserRepositoryIT.java
+++ src/test/java/com/emil/cicd/repository/UserRepositoryIT.java
@@ -4,17 +4,35 @@ import com.emil.cicd.entity.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@TestPropertySource(locations = "classpath:application-local.properties")
-@DataJpaTest // pornește doar partea de JPA (nu tot contextul Spring Boot)
+@Testcontainers
 class UserRepositoryIT {
+
+    @Container
+    static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8")
+            .withDatabaseName("demo_db")
+            .withUsername("root")
+            .withPassword("emil");
+
+    @DynamicPropertySource
+    static void properties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mysql::getJdbcUrl);
+        registry.add("spring.datasource.username", mysql::getUsername);
+        registry.add("spring.datasource.password", mysql::getPassword);
+    }
 
     @Autowired
     private UserRepository userRepository;
@@ -26,17 +44,13 @@ class UserRepositoryIT {
 
     @Test
     void testSaveAndFindAll() {
-        // given
         User user = new User();
         user.setName("Alice");
 
-        // when
         userRepository.save(user);
         List<User> users = userRepository.findAll();
 
-        // then
         assertThat(users).hasSize(1);
         assertThat(users.get(0).getName()).isEqualTo("Alice");
     }
 }
-
