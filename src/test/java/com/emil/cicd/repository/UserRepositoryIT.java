63f11d83f9eb56bd7ddf374843e5ab82a3700656
Emil Popa <emilpopa24@yahoo.com>
2025-09-20 18:14:14 +0300

    some adjustments regarding IT

--- src/test/java/com/emil/cicd/repository/UserRepositoryIT.java
+++ src/test/java/com/emil/cicd/repository/UserRepositoryIT.java
@@ -1,19 +1,28 @@
 package com.emil.cicd.repository;
 
 import com.emil.cicd.entity.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.TestPropertySource;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@TestPropertySource(locations = "classpath:application-local.properties")
 @DataJpaTest // pornește doar partea de JPA (nu tot contextul Spring Boot)
 class UserRepositoryIT {
 
     @Autowired
     private UserRepository userRepository;
+
+    @BeforeEach
+    void cleanDb() {
+        userRepository.deleteAll();
+    }
 
     @Test
     void testSaveAndFindAll() {
